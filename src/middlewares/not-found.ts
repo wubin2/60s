17ead0fc95079c32be97e98330b1f565390f55c5
Tristan <wubin@coolsong.com>
2024-05-17 19:51:25 +0800

    Update not-found.ts

--- src/middlewares/not-found.ts
+++ src/middlewares/not-found.ts
@@ -3,7 +3,7 @@ import { Context, Next } from 'oak'
 export default async function notFound(ctx: Context, next: Next) {
   await next()
 
-  ctx.response.redirect('https://github.com/vikiboss/60s')
+  ctx.response.redirect('https://60s.coolsong.com')
 
   return
 }
